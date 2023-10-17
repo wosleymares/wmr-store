499be2bd7ba8299be74993c0f0c342d4b1efdfae
wosleymares <wesleyaddan@gmail.com>
2023-10-17 19:26:37 -0300

    chore: adds shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div  
-      className="bg-blue-500 text-red-500"
-      >
-        hello world
+    <div >
     </div>
   );
 }
